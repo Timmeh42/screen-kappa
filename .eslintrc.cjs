91b28f94691c4af9d8a7f8e2e18fb85deebc0a67
timmeh42 <tmeier42+github@gmail.com>
2024-01-01 12:04:25 +0200

    linting config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,27 +2,31 @@
 require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
-    root: true,
-    extends: [
-        'plugin:vue/vue3-essential',
-        'eslint:recommended',
-        'plugin:@stylistic/all-extends',
-        '@vue/eslint-config-typescript',
-    ],
-    parserOptions: {
-        ecmaVersion: 'latest',
-    },
-    plugins: ['@stylistic'],
-    rules: {
-        '@stylistic/array-element-newline': ['error', 'consistent'],
-        '@stylistic/comma-dangle': ['error', 'always-multiline'],
-        '@stylistic/dot-location': ['error', 'property'],
-        '@stylistic/function-call-argument-newline': ['error', 'consistent'],
-        '@stylistic/max-len': ['off'],
-        '@stylistic/multiline-ternary': ['error', 'always-multiline'],
-        '@stylistic/object-curly-spacing': ['error', 'always'],
-        '@stylistic/padded-blocks': ['error', 'never'],
-        '@stylistic/quote-props': ['error', 'as-needed'],
-        '@stylistic/quotes': ['error', 'single'],
-    },
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    'plugin:@stylistic/all-extends',
+    'plugin:vue/vue3-recommended',
+    '@vue/eslint-config-typescript',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  plugins: ['@stylistic'],
+  rules: {
+    '@stylistic/array-bracket-newline': ['error', 'consistent'],
+    '@stylistic/array-element-newline': ['error', 'consistent'],
+    '@stylistic/comma-dangle': ['error', 'always-multiline'],
+    '@stylistic/dot-location': ['error', 'property'],
+    '@stylistic/function-call-argument-newline': ['error', 'consistent'],
+    '@stylistic/indent': ['error', 2],
+    '@stylistic/max-len': ['off'],
+    '@stylistic/multiline-ternary': ['error', 'always-multiline'],
+    '@stylistic/object-curly-spacing': ['error', 'always'],
+    '@stylistic/padded-blocks': ['error', 'never'],
+    '@stylistic/quote-props': ['error', 'as-needed'],
+    '@stylistic/quotes': ['error', 'single'],
+    '@stylistic/spaced-comment': ['error', 'always', { markers: ['/'] }],
+  },
 };
